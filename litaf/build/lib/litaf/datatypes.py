e14d86193dfa41041799a957d592a84b44edf99c
Luca Chiesa <lchiesa@palladium.ccm.lit.unistra.fr>
2024-07-16 15:07:37 +0200

    removed build files

--- litaf/build/lib/litaf/datatypes.py
+++
@@ -1,7 +0,0 @@
-from typing import MutableMapping, Union
-from alphafold.data.tools import hhsearch
-from alphafold.data.tools import hmmsearch
-import numpy as np
-
-FeatureDict = MutableMapping[str, np.ndarray]
-TemplateSearcher = Union[hhsearch.HHSearch, hmmsearch.Hmmsearch]
